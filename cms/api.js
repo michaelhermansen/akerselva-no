2f94ca46703bac083ba88aec5e574a79e53c5359
Michael Hermansen <michael.p.hermansen@gmail.com>
2022-08-18 17:38:05 +0200

    Configure thing

---
+++ cms/api.js
@@ -0,0 +1,34 @@
+export default {
+  "landing-page-sections": {
+    data: [
+      {
+        id: 1,
+        attributes: {
+          Title: "Rutebåt mellom Vaterland og Bjørvika",
+          Subtitle: "Aktivitet Vår / Sommer",
+          ImageTitle: "Opplev Akerselvas nye rutebåt, gjennom kulverten.",
+          Description:
+            "Under togsporene på Oslo S, mellom Vaterland og Bjørvika ligger Akerselva i kulvert. Vi jobber for å tilgjengeliggjøre den siste delen av Akerselva gjennom kunst- og kulturopplevelser!",
+          Link: {
+            LinkText: "Se rutetider",
+            LinkUrl: "https://google.com",
+          },
+        },
+      },
+      {
+        id: 2,
+        attributes: {
+          Title: "Inger Munch og Akerselva",
+          Subtitle: "Digital utstilling",
+          ImageTitle: "",
+          Description:
+            "I 1927 fikk Inger et kamera av sin bror Edvard Munch og med tiden ble hun en flink fotograf som på oppfordring av sin bror fotograferte akerselven fra Maridalen i nord til kysten i sør. I vår digitale utstilling kan du oppleve Akerselva slik Inger Munch så den.",
+          Link: {
+            LinkText: "Se digital utstilling",
+            LinkUrl: "https://google.com",
+          },
+        },
+      },
+    ],
+  },
+};
