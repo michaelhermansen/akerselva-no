a68668ae922170192becd5a2adf0e5a2935f4f4e
Michael Hermansen <michael.p.hermansen@gmail.com>
2022-08-26 15:33:40 +0200

    Fixed too many map box requests

--- site/src/components/modules/Exhibition/ExhibitionScroller/Scroller.tsx
+++ site/src/components/modules/Exhibition/ExhibitionScroller/Scroller.tsx
@@ -9,6 +9,7 @@ import FrameContent from "./FrameContent";
 import ImageFocus from "./ImageFocus";
 import ScrollerText from "./ScrollerText";
 import NoSSR from "react-no-ssr";
+import classNames from "classnames";
 
 export default function ImageScroller() {
   const [frameRef, frameBounds] = useMeasure();
@@ -59,28 +60,23 @@ export default function ImageScroller() {
         </NoSSR>
 
         {/* Fixed frame on mobile */}
-        <AnimatePresence>
-          {inView && (
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{ type: "tween", duration: 0.2 }}
-              className="fixed bottom-6 right-6 z-50 h-[220px] w-3/4 flex-1 overflow-hidden rounded-sm bg-gray-medium shadow-2xl sm:h-[250px] sm:w-2/3 md:hidden"
-            >
-              <FrameContent showMap={showMap} itemInView={itemInView} />
-
-              <button
-                onClick={toggleMap}
-                className="absolute top-4 right-4 z-50 grid h-12 w-12 place-items-center rounded-full bg-black bg-opacity-75 text-white text-opacity-75 shadow-lg backdrop-blur-lg transition-colors hover:text-opacity-100"
-                title={showMap ? "Se bilde" : "Se kart"}
-              >
-                {showMap && <MdCameraAlt size={24} />}
-                {!showMap && <MdLocationPin size={24} />}
-              </button>
-            </motion.div>
+        <div
+          className={classNames(
+            "fixed bottom-6 right-6 z-50 h-[220px] w-3/4 flex-1 overflow-hidden rounded-sm bg-gray-medium shadow-2xl transition-opacity sm:h-[250px] sm:w-2/3 md:hidden",
+            { "pointer-events-none opacity-0": !inView }
           )}
-        </AnimatePresence>
+        >
+          <FrameContent showMap={showMap} itemInView={itemInView} />
+
+          <button
+            onClick={toggleMap}
+            className="absolute top-4 right-4 z-50 grid h-12 w-12 place-items-center rounded-full bg-black bg-opacity-75 text-white text-opacity-75 shadow-lg backdrop-blur-lg transition-colors hover:text-opacity-100"
+            title={showMap ? "Se bilde" : "Se kart"}
+          >
+            {showMap && <MdCameraAlt size={24} />}
+            {!showMap && <MdLocationPin size={24} />}
+          </button>
+        </div>
 
         {/* Text scroller */}
         <div
