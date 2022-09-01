bdfe6612eae3987e27a89ef3340fbeb7b67b0a30
Michael Hermansen <michael.p.hermansen@gmail.com>
2022-09-02 01:04:07 +0200

    image focus fixes

--- site/src/components/modules/Exhibition/ExhibitionScroller/ImageFocus.tsx
+++ site/src/components/modules/Exhibition/ExhibitionScroller/ImageFocus.tsx
@@ -31,7 +31,7 @@ export default function ImageFocus({ selectedItem }: ImageFocusProps) {
       const listItem: HTMLLIElement | null = document.querySelector(
         `#image-${selectedItem.id}`
       );
-      return (listItem?.offsetTop || 0) - window.innerHeight * 0.4 + 8;
+      return (listItem?.offsetTop || 0) - window.innerHeight * 0.4;
     }
 
     window.scroll({ top: getListItemScrollPos() });
@@ -157,9 +157,9 @@ export default function ImageFocus({ selectedItem }: ImageFocusProps) {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ duration: 0.2 }}
-      className="fixed inset-0 z-[99] flex items-center justify-center gap-10 overflow-y-scroll overscroll-y-contain bg-black px-8 pt-8 pb-56 md:pb-8"
+      className="fixed inset-0 isolate z-[99] flex items-center justify-center gap-10 overflow-y-scroll overscroll-y-contain bg-black px-8 pt-8 pb-56 md:pb-8"
     >
-      <div className="fixed bottom-12 z-10 -translate-x-14 md:relative md:translate-x-0">
+      <div className="fixed bottom-12 z-30 -translate-x-14 md:relative md:translate-x-0">
         <button
           disabled={selectedItem.id === scrollerItems[0].id}
           className="rounded-full bg-gray-medium p-6 text-white transition-colors active:bg-opacity-75 disabled:cursor-not-allowed disabled:opacity-40"
@@ -179,7 +179,7 @@ export default function ImageFocus({ selectedItem }: ImageFocusProps) {
           exit="hidden"
           className="w-full max-w-6xl"
         >
-          <div className="relative grid aspect-square max-h-[70vh] w-full place-items-center rounded-md border border-gray-medium bg-black">
+          <div className="relative grid aspect-square max-h-[60vh] w-full place-items-center rounded-md border border-gray-medium bg-black md:max-h-[70vh]">
             <Image
               src={`/assets/exhibition-scroller/${selectedItem.id}.jpg`}
               alt={selectedItem.text}
@@ -188,13 +188,21 @@ export default function ImageFocus({ selectedItem }: ImageFocusProps) {
               className="object-contain"
             />
           </div>
-          <p className="mx-auto max-w-3xl px-2 pt-6 text-center text-white text-opacity-75">
-            {selectedItem.text}
-          </p>
+          <div className="relative z-20 flex items-baseline justify-between gap-6 px-2 pt-6">
+            <p className="max-w-3xl  text-white text-opacity-75">
+              {selectedItem.text}
+            </p>
+            <button
+              onClick={closeFocus}
+              className="rounded-full border border-gray-medium px-6 py-2 text-white text-opacity-60 transition-colors hover:text-opacity-100"
+            >
+              Lukk
+            </button>
+          </div>
         </motion.div>
       </AnimatePresence>
 
-      <div className="fixed bottom-12 z-10 translate-x-14 md:relative md:translate-x-0">
+      <div className="fixed bottom-12 z-30 translate-x-14 md:relative md:translate-x-0">
         <button
           disabled={
             selectedItem.id === scrollerItems[scrollerItems.length - 1].id
